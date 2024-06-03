1c6ea57fbd3741ea738e5ac2ab731a36e66397dd
BEN LAAZIZ HAMZA <benlaaziz.hamza@gmail.com>
2024-06-03 17:57:40 +0100

    update functions builtins

--- cd.c
+++ cd.c
@@ -6,39 +6,48 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 18:39:55 by hben-laz          #+#    #+#             */
-/*   Updated: 2024/06/03 17:06:05 by hben-laz         ###   ########.fr       */
+/*   Updated: 2024/06/03 17:55:56 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	change_env(t_env **env, t_env *home, char *oldpwd)
+{
+	char	*pwd;
+	
+	home = point_node(*env, "OLDPWD");
+	if (home)
+		home->value = oldpwd;
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+		return (perror(""));
+	home = point_node(*env, "PWD");
+	if (home)
+		home->value = pwd;
+}
 
 void	cd(char **cmd, t_env **env)
 {
 	t_env	*home;
 	char	*oldpwd;
-	char	*pwd;
 	
+	home = NULL;
 	oldpwd = getcwd(NULL, 0);
-	if (oldpwd == NULL)
+	if (!oldpwd)
 		return (perror(""));
 	if (cmd[1] == NULL)
 	{
 		home = point_node(*env, "HOME");
 		if (!home)
 			return ;
-		chdir(home->value);
+		if (chdir(home->value) != 0)
+			perror(home->value);
 	}
 	else
 	{
 		if (chdir(cmd[1]) != 0)
 			perror(cmd[1]);
 	}
-	home = point_node(*env, "OLDPWD");
-	if (home)
-		home->value = oldpwd;
-	pwd = getcwd(NULL, 0);
-	home = point_node(*env, "PWD");
-	if (home)
-		home->value = pwd;
+	change_env(env, home, oldpwd);
 }
